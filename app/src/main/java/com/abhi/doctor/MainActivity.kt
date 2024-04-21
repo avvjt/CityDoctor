817dd7522eff4750b9826ce2d5f357e7f815a3ce
Abhi <avvjit@gmail.com>
2024-04-21 18:49:32 +0530

    BottomNavigation implement with fragments

--- app/src/main/java/com/abhi/doctor/MainActivity.kt
+++ app/src/main/java/com/abhi/doctor/MainActivity.kt
@@ -6,8 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Thread.sleep(2000)
@@ -15,5 +20,31 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
+        bottomNavigationView = findViewById(R.id.bottomNavigation)
+        bottomNavigationView.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.home -> {
+                    replaceFragment(Home())
+                    true }
+                R.id.bookings -> {
+                    replaceFragment(Bookings())
+                    true }
+                R.id.offers -> {
+                    replaceFragment(Offers())
+                    true }
+                R.id.cart -> {
+                    replaceFragment(Cart())
+                    true }
+                R.id.account -> {
+                    replaceFragment(Account())
+                    true }
+                else -> false
+            }
+        }
+        replaceFragment(Home())
+
+    }
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.frameLayout, fragment).commit()
     }
 }
